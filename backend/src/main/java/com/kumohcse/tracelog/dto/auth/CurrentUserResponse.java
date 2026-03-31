586afd0ad396313786d8777d62401f6a08819ad7
박진호 <pjho501137@gmail.com>
2026-03-31 16:11:15 +0900

    login, logout

---
+++ backend/src/main/java/com/kumohcse/tracelog/dto/auth/CurrentUserResponse.java
@@ -0,0 +1,6 @@
+package com.kumohcse.tracelog.dto.auth;
+
+public record CurrentUserResponse(
+    AuthUserResponse user
+) {
+}
--- backend/src/main/java/com/kumohcse/tracelog/repository/UserRepository.java
+++ backend/src/main/java/com/kumohcse/tracelog/repository/UserRepository.java
@@ -11,4 +11,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    boolean existsByEmailIgnoreCase(String email);
 }
